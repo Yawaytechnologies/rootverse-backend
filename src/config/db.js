d9909936b1b59c436a007aeb6f18c873d00dcd35
Yawaytech <career.yaway@gmail.com>
2025-12-11 12:47:50 +0530

    Initial commit

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import config from "./env.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.dbUrl);
+    console.log("✨-----MongoDB connected successfully-----✨");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/config/env.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  port: process.env.PORT ,
+  dbUrl: process.env.DB_URL ,
+};
+
+export default config;
