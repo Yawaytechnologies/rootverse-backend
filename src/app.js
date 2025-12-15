90823aee3f55e86a26600faf08e7423211830043
Praveenkumar-6001 <praveen04072025@gmail.com>
2025-12-15 10:04:22 +0530

    change the mongo db into sql based progress

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import pool from './config/db.js';
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ success: true, time: result.rows[0] });
+  } catch (error) {
+    console.error('Database connection error:', error);
+    
+    res.status(500).json({ success: false, message: 'Database connection error' });
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Rootverse Backend is running');
--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  port: process.env.PORT ,
-  dbUrl: process.env.DB_URL ,
+  port: process.env.PORT,
+  DB_host: process.env.PG_HOST,
+  DB_port: Number(process.env.PG_PORT),
+  DB_user: process.env.PG_USER,
+  DB_password: process.env.PG_PASSWORD,
+ DB_database: process.env.PG_DATABASE,
+  
 };
 
 export default config;
