7091c75c0a0da63458588c8b4269b42dffe4dca4
ThushyanthanJayakumar <thushyanthanbackend@gmail.com>
2026-01-02 16:39:26 +0530

    vessel_reg

---
+++ migrations/20251230071706_create_vessel_regiter.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable("vessel_registration", (table) => {
+    table.increments("id").primary();
+    table.string("rv_vessel_id",15).notNullable().unique();
+    table.string("govt_registration_number",15).notNullable();
+    table.string("local_identifier",60).nullable();
+    table.string("vessel_name",50).notNullable();
+    table.string("home_port",80).notNullable();
+    table.string("vessel_type",50).notNullable();
+    table.text("allowed_fishing_methods").nullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
+    table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable();
+    table.index(["rv_vessel_id"], "vessel_registration_rv_vessel_id_index");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+export async function down(knex) {
+  await knex.schema.dropTableIfExists("vessel_registration");
+}
+
