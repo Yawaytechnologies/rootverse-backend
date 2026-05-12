ece1a89694a4e6420ef0552d2e190edcad0aae1c
Praveenkumar-6001 <praveen04072025@gmail.com>
2026-05-12 12:05:35 +0530

    cultures_cycle modification

--- src/modules/aquaculture/cultures_cycle/cultures_cycle_routes.js
+++ src/modules/aquaculture/cultures_cycle/cultures_cycle_routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { createCultureCycleController, getCultureCycleByIdController, getCultureCycleByUserIdController, getCultureCyclesByVerificationStatusController, updateVerificationStatusController } from "./cultures_cycle_controller.js";
+import { createCultureCycleController, getAllCultureCyclesController, getCultureCycleByIdController, getCultureCycleByUserIdController, getCultureCyclesByFarmIdAndPondIdController, getCultureCyclesByFarmIdController, getCultureCyclesByVerificationStatusController, updateVerificationStatusController } from "./cultures_cycle_controller.js";
 
 const router = express.Router();
 
 router.post("/", createCultureCycleController);
+router.get("/", getAllCultureCyclesController);
 router.get("/user/:user_id", getCultureCycleByUserIdController);
+router.get("/farm/:farm_id/pond/:pond_id", getCultureCyclesByFarmIdAndPondIdController);
+router.get("/farm/:farm_id", getCultureCyclesByFarmIdController);
 router.get("/verification-status/:verification_status", getCultureCyclesByVerificationStatusController);
 router.get("/:id", getCultureCycleByIdController);
 router.put("/:id/verification-status", updateVerificationStatusController);
